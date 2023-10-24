948e14ce595b084f13e543b7b56caa4d8d16c148
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-10-24 15:58:00 -0300

    refact install packages script

--- install-packages.sh
+++ install-packages.sh
@@ -12,29 +12,13 @@ export CHECK="\xE2\x9C\x85"
 && chsh -s /bin/zsh \
     && echo -e "\n $CHECK zsh set as default shell" \
 
-# java
-&& brew install openjdk \
-    && export JAVA_HOME=$(/usr/libexec/java_home -v 11) \
-    && export PATH=$JAVA_HOME/bin:$PATH \
-    && echo -e "\n $CHECK  java installed" \
-
-# node version mannager
-&& brew install nvm \
-    && echo -e "\n $CHECK nvm installed" \
-
-# vim
-&& brew install vim \
-    && echo -e "\n $CHECK vim installed" \
-
-# gh cli tool
-&& brew install gh \
-    && echo -e "\n $CHECK gh installed" \
+# packages
+&& brew install openjdk nvm vim gh colima docker \
+&& brew install --cask mysqlworkbench firefox postman visual-studio-code warp zulu11 \
+&& echo -e "\n $CHECK packages installed" \
 
 # go version mannager
 && bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) \
-    && source /Users/kcalixto/.gvm/scripts/gvm \
-    && gvm install go1.19 \
-    && gvm use go1.19 --default \
-    && export GOPATH=$HOME/go \
-    && export PATH=$PATH:$GOPATH/bin \    
-    && echo -e "\n $CHECK go installed" \
+&& source /Users/kcalixto/.gvm/scripts/gvm \
+&& gvm install go1.19 \
+&& gvm use go1.19 --default
