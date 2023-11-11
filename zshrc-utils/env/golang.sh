e781fc4ca776f0acd33ad619a524efb8ef94b97a
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-11-11 02:05:21 -0300

    wtf

--- zshrc-utils/env/golang.sh
+++ zshrc-utils/env/golang.sh
@@ -1,4 +1,4 @@
 # go version mannager
 # gvm use go1.19 --default
 [[ -s "/Users/kaua.calixto/.gvm/scripts/gvm" ]] && source "/Users/kaua.calixto/.gvm/scripts/gvm"
-export GOPATH=$GOPATH:$HOME/go
+export GOPATH=$GOPATH:$HOME/go/bin
