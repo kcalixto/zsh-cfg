a3d6b831f1d53337df3084d298c76c3c4809e01d
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-06-12 15:30:12 -0300

    chore: Update GOPRIVATE environment variable in golang.sh

--- zshrc-utils/env/golang.sh
+++ zshrc-utils/env/golang.sh
@@ -4,6 +4,7 @@
 
 export GOROOT=/Users/kaua.calixto/.gvm/gos/go1.19
 export GOROOT_BOOTSTRAP=$GOROOT
+export GOPRIVATE=github.com # don't forget to add ssh over https in .gitconfig
 
 export GOPATH=$HOME/go/src
 
