71a613fe6c575619cc7365ba56d8cac8e6ffd924
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-09-09 19:23:21 -0300

    git aliases fix

--- scripts/git-status-scan.sh
+++ scripts/git-status-scan.sh
@@ -3,15 +3,7 @@ BLUE='\033[0;34m'
 git-scan() {
     my_projects_folder="$HOME/go/src"
 
-    cd $my_projects_folder
-    FOLDERS=$(ls -d */)
-    for folder in $FOLDERS; do
-        cd $folder
-        echo -e "${BLUE} $folder"
-        git status -s
-        cd ..
+    for file in `find $my_projects_folder -type d -maxdepth 2` ; do
+        (cd $file && git cherry -v)
     done
-
-    # goes back to original dir
-    cd -
 }
