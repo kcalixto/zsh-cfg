c399297f9ac4f80a66eaded53f31afd8aad3b388
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-09-09 19:23:26 -0300

    fix go env

--- zshrc-utils/env/golang.sh
+++ zshrc-utils/env/golang.sh
@@ -2,8 +2,8 @@
 # gvm use go1.19 --default
 [[ -s "/Users/kaua.calixto/.gvm/scripts/gvm" ]] && source "/Users/kaua.calixto/.gvm/scripts/gvm"
 
-export GOROOT=/Users/kaua.calixto/.gvm/gos/go1.19
-export GOROOT_BOOTSTRAP=$GOROOT
+# export GOROOT=/Users/kaua.calixto/.gvm/gos/go1.19
+# export GOROOT_BOOTSTRAP=$GOROOT
 # export GOPRIVATE=github.com # don't forget to add ssh over https in .gitconfig
 
 export GOPATH=$HOME/go/src
