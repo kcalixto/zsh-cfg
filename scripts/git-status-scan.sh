5dc72d43a3f8493fe49ce3da047607221e56e0fd
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-10-17 11:41:36 -0300

    some aws useful scripts

--- scripts/git-status-scan.sh
+++ scripts/git-status-scan.sh
@@ -3,7 +3,8 @@ BLUE='\033[0;34m'
 git-scan() {
     my_projects_folder="$HOME/go/src"
 
-    for file in `find $my_projects_folder -type d -maxdepth 2` ; do
+    for file in `find $my_projects_folder -type d -maxdepth 1` ; do
+        echo -e "${BLUE}Scanning $file"
         (cd $file && git cherry -v)
     done
 }
