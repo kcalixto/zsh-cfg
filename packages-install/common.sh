abd85825079b80d9344747baa413a6ad1871889e
Kauã Chaves Calixto <80490049+kcalixto@users.noreply.github.com>
2024-10-22 11:10:28 -0300

    Update common.sh

--- packages-install/common.sh
+++ packages-install/common.sh
@@ -1,5 +1,5 @@
 export CHECK="\xE2\x9C\x85"
 
-brew install vim nvim gh colima docker awscli aws-sam-cli fzf \
-&& brew install --cask mysqlworkbench firefox postman visual-studio-code warp discord \
+brew install vim nvim gh orbstack awscli aws-sam-cli fzf \
+&& brew install --cask mysqlworkbench firefox postman visual-studio-code alacritty discord \
 && echo -e "\n $CHECK common packages & applications installed!"
