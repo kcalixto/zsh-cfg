58eb49b308e345c0aa4b64bc3a5712cdbfff2e15
Kauã Chaves Calixto <80490049+kcalixto@users.noreply.github.com>
2024-10-22 11:15:16 -0300

    Update golang.sh

--- packages-install/golang.sh
+++ packages-install/golang.sh
@@ -1,7 +1,7 @@
 export CHECK="\xE2\x9C\x85"
 
 # go version mannager
-&& bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) \
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) \
 && source ~/.gvm/scripts/gvm \
 && gvm install go1.23.0 \
 && gvm use go1.23.0 --default
