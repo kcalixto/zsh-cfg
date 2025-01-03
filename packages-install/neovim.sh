c4b47d962b56d6c7598ff9790a28ce6049b95e58
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2025-01-03 00:31:12 -0300

    added nvim required packages

---
+++ packages-install/neovim.sh
@@ -0,0 +1,3 @@
+brew install ripgrep ninja cmake gettext curl zsh-syntax-highlighting tmux tree fd
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+brew install --cask iterm2
