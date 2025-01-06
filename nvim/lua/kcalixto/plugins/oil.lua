82e4105a938387045d7021de4a5f1f6f00fc4218
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2025-01-05 21:19:03 -0300

    added new oil custom keybind

--- nvim/lua/kcalixto/plugins/oil.lua
+++ nvim/lua/kcalixto/plugins/oil.lua
@@ -1,11 +1,21 @@
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set('n', '<leader>s',
-  function()
-    vim.cmd('w') -- same as :w
-    vim.notify('File saved successfully', vim.log.levels.INFO)
-  end,
-  { desc = "Format and save the file" }
-)
+
+-- haha no way it works LOL
+local winid = -1
+vim.keymap.set('n', '<space>e', function()
+  if vim.g.kcalixto_oil_open == true then
+    vim.g.kcalixto_oil_open = false
+    vim.api.nvim_win_close(winid, false)
+  else
+    vim.g.kcalixto_oil_open = true -- toggle var hehe
+
+    vim.cmd.vnew() -- new window
+    vim.cmd('Oil') -- opens oil of course
+    vim.cmd.wincmd('H') -- mv window to far left
+    vim.api.nvim_win_set_width(0, 35)
+    winid = vim.fn.win_getid() -- saves id for close later
+  end
+end)
 
 return {
   {
