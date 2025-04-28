82d0907a56fb9e296ad7fdd6b862ec143bdeedb9
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2025-04-28 16:55:02 -0300

    added z extension

--- packages-install/common.sh
+++ packages-install/common.sh
@@ -1,5 +1,5 @@
 export CHECK="\xE2\x9C\x85"
 
-brew install vim nvim gh orbstack awscli aws-sam-cli fzf \
+brew install vim nvim gh orbstack awscli aws-sam-cli fzf z \
 && brew install --cask mysqlworkbench firefox postman visual-studio-code alacritty discord \
 && echo -e "\n $CHECK common packages & applications installed!"
