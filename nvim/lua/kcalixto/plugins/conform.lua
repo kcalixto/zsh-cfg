944235e93a066b90b75061d2e5bd11a45ff69594
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2025-05-22 15:10:48 -0300

    adjust nvim plugins

---
+++ nvim/lua/kcalixto/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'stevearc/conform.nvim',
+
+    event = "BufWritePre",
+    opts = {},
+    config = function()
+      require("conform").setup({
+        formatters = {
+          gofmt = { timeout_ms = 500 },
+        },
+        formatters_by_ft = {
+          javascript = { "prettier", stop_after_first = true },
+          scss = { "prettier", stop_after_first = true },
+          typescriptreact = { "prettier", stop_after_first = true },
+          go = { "goimports", "gofmt" },
+          rust = { "rustfmt" },
+          python = { "black" },
+          cpp = { "clang-formatter" },
+        },
+        format_on_save = false,
+      })
+    end
+  },
+}
