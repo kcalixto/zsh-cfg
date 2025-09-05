a0f94449b7f1a8b6d41858bcc5ab84aed8a2f23c
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2025-09-05 20:56:07 -0300

    chore: update common package install script

--- packages-install/common.sh
+++ packages-install/common.sh
@@ -1,5 +1,5 @@
 export CHECK="\xE2\x9C\x85"
 
-brew install vim nvim gh orbstack awscli aws-sam-cli fzf z \
-&& brew install --cask mysqlworkbench firefox postman visual-studio-code alacritty discord \
+brew install vim nvim gh tmux ripgrep orbstack awscli aws-sam-cli fzf z bat \
+&& brew install --cask arc postman visual-studio-code iterm2 discord \
 && echo -e "\n $CHECK common packages & applications installed!"
